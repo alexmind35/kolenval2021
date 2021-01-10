2bb880a09620fe44b93809674419d125beffd911
Alexander Mikhaylov <alexmind@yandex.ru>
2021-01-10 22:33:40 +0300

    Создан суперюзер. Поправлены модели Order и Service

--- orders/models.py
+++ orders/models.py
@@ -4,8 +4,17 @@ from django.db import models
 User = get_user_model()
 
 
+class Service(models.Model):
+    name_service = models.CharField(max_length=255)
+    price = models.PositiveIntegerField("Цена")
+
+    def __str__(self):
+        return f'Service: {self.name_service}, {self.price}'
+
+
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     date_order = models.DateTimeField(auto_now_add=True)
     time_order = models.TimeField(auto_now_add=True)
     STATUS_ORDERS = [
@@ -16,13 +25,4 @@ class Order(models.Model):
     status_order = models.CharField(choices=STATUS_ORDERS, default='1', max_length=1)
 
     def __str__(self):
-        return f'User: {self.user}, date_order: {self.date_order}, time_order: {self.time_order}, status: {self.get_status_order_display()} '
-
-
-class Service(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    name_service = models.CharField(max_length=255)
-    price = models.PositiveIntegerField("Цена")
-
-    def __str__(self):
-        return f'Order: {self.order}, name_service: {self.name_service}, price: {self.price}'
+        return "User: {}, Service: {}, status_order: {}".format(self.user, self.service, self.get_status_order_display())
