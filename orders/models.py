f0047d5c36e6de060176b9db6bccbbf50a2b682f
Alexander Mikhaylov <alexmind@yandex.ru>
2021-01-10 21:57:52 +0300

    Созданы модели Order, Service в приложении Orders. Выполнены миграции

---
+++ orders/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_order = models.DateTimeField(auto_now_add=True)
+    time_order = models.TimeField(auto_now_add=True)
+    STATUS_ORDERS = [
+        ('1', 'Новый'),
+        ('2', 'Отменен'),
+        ('3', 'Выполнен')
+    ]
+    status_order = models.CharField(choices=STATUS_ORDERS, default='1', max_length=1)
+
+    def __str__(self):
+        return f'User: {self.user}, date_order: {self.date_order}, time_order: {self.time_order}, status: {self.get_status_order_display()} '
+
+
+class Service(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    name_service = models.CharField(max_length=255)
+    price = models.PositiveIntegerField("Цена")
+
+    def __str__(self):
+        return f'Order: {self.order}, name_service: {self.name_service}, price: {self.price}'
