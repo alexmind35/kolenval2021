617dcfe2b4d741d83e03d5ce6f497d042727cea5
Alexander Mikhaylov <alexmind@yandex.ru>
2021-01-11 08:22:15 +0300

    Создан суперюзер. Поправлены модели Order и Service

--- orders/models.py
+++ orders/models.py
@@ -4,17 +4,8 @@ from django.db import models
 User = get_user_model()
 
 
-class Service(models.Model):
-    name_service = models.CharField(max_length=255)
-    price = models.PositiveIntegerField("Цена")
-
-    def __str__(self):
-        return f'Service: {self.name_service}, {self.price}'
-
-
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    service = models.ForeignKey(Service, on_delete=models.CASCADE)
     date_order = models.DateTimeField(auto_now_add=True)
     time_order = models.TimeField(auto_now_add=True)
     STATUS_ORDERS = [
@@ -25,4 +16,13 @@ class Order(models.Model):
     status_order = models.CharField(choices=STATUS_ORDERS, default='1', max_length=1)
 
     def __str__(self):
-        return "User: {}, Service: {}, status_order: {}".format(self.user, self.service, self.get_status_order_display())
+        return f'User: {self.user}, {self.get_status_order_display()}'
+
+
+class Service(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    name_service = models.CharField(max_length=255)
+    price = models.PositiveIntegerField("Цена")
+
+    def __str__(self):
+        return f'Order: {self.order}, {self.name_service}, {self.price}'
